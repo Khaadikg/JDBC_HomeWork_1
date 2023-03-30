8864274639186efe5c385830e1e6aea7fd75a04b
Khaadikg <126019047+Khaadikg@users.noreply.github.com>
2023-03-30 07:27:09 +0300

    Update DB.java

--- JdbcHomeWork/src/main/java/org/peaksoft/DB.java
+++ JdbcHomeWork/src/main/java/org/peaksoft/DB.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DB {
     private static final String URL = "jdbc:postgresql://localhost:5432/jdbc_task_1";
-    private static final String User = "haadibolotbekov";
-    private static final String Pass = "KillPandas01";
+    private static final String User = "your login";
+    private static final String Pass = "your password";
 
     public static Connection connection (){
         Connection connection = null;
